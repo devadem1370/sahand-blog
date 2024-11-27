13f41f21e839f5ba359e5ef4a605f930e2e5c3a5
Adil Imran <devadem1370@gmail.com>
2024-11-27 07:17:11 -0800

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -32,3 +33,38 @@ export const signup = async (req, res, next) => {
     return res.status(500).json({ message: 'Server error. Please try again later.' });
   }
 };
+
+
+//signin controller
+
+export const signin = async (req,res,next) =>{
+  const {email,password} = req.body;
+if(!email || !password ||  password == "") {
+  next(errorHandler(400, "All the fields are required"))
+}
+
+
+try{
+  
+const validUser = await User.findOne({email}) ;
+if(!validUser){
+ return next(errorHandler(404,"User not found"))
+}
+
+const validPassword =  bcryptjs.compareSync(password, validUser.password)
+if(!validPassword){
+ return next(errorHandler(404,"Invalid Credentials"))
+}
+const token = jwt.sign(
+{id: validUser._id} , process.env.JWT_SECRET);
+const {password:pass, ...rest}= validUser._doc;
+
+  res.status(200).cookie('access_token',token,{
+    httpOnly:true}).json(rest)
+
+
+}catch(error){
+  next(error)
+}
+
+}
